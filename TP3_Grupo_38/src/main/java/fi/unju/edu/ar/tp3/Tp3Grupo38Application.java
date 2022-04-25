e130215d6e4d58765b7113b61a8586117970faff
GTL <GTL@DESKTOP-0LQB8T5>
2022-04-24 22:26:11 -0300

    Primer Commit

---
+++ TP3_Grupo_38/src/main/java/fi/unju/edu/ar/tp3/Tp3Grupo38Application.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.tp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3Grupo38Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3Grupo38Application.class, args);
+	}
+
+}
---
+++ TP3_Grupo_38/src/test/java/fi/unju/edu/ar/tp3/Tp3Grupo38ApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.tp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3Grupo38ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
